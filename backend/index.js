3efeb3d26cb2fc5b2a510a97f7cbfb94bc179a1a
TJ <tjbindseil@gmail.com>
2022-08-21 20:06:32 -0600

    get bare bones skeleton of very simple backend server

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,53 @@
-// http is an inbuilt module in Node.js
 const http = require('http');
 
-// createServer method takes a callback function as argument
-// the callback function takes two arguments req and re
+    // I guess I can start with a server that can:
+    // 1. receive updates and apply them to a master thing
+    // 2. propagate updates
+    // 3. register and unregister clients from photos
+    // 4. send png of picture upon registration
+    // 5. list pictures
+    // 6. create pictures
+
+    //
+    // hmm, there is a crud app here and a registration service
+    // i say this because the methods and routes are hard to name
+    // no need to overthink it
+    //
+
 const server = http.createServer(function (req, res) {
     res.statusCode = 200; // 200 = OK
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Demo page</h1>");
-    res.end();
+    res.setHeader('Content-Type', 'application/json');
+
+    console.log(`${req.method} request received at ${req.url}`);
+    // TODO how to deal with <picture_id> in routes?
+    // TODO how to deal with req.method === 'POST'
+    if (req.url === '/<picture_id>/update' && req.method === 'POST') {
+        // TODO update a reference picture and send to all registered clients
+        res.write(JSON.stringify({'msg': 'received update'}));
+        res.end();
+    } else if (req.url === '/<picture_id>/clients' && req.method === 'PUT') {
+        // TODO register a client, send them reference photo and subscribe them to updates
+        res.write(JSON.stringify({'msg': 'client added to picture <picture_id>'}));
+        res.end();
+    } else if (req.url === '/<picture_id>/clients' && req.method === 'DELETE') {
+        // TODO unregister a client
+        res.write(JSON.stringify({'msg': 'client removed from picture <picture_id>'}));
+        res.end();
+    } else if (req.url === '/pictures' && req.method === 'GET') {
+        // TODO return list of all photos by name and maybe include a thumbnail
+        res.write(JSON.stringify({'msg': 'all pictures'}));
+        res.end();
+    } else if (req.url === '/pictures' && req.method === 'POST') {
+        // TODO create picture with name and other details
+        res.write(JSON.stringify({'msg': 'picture created'}));
+        res.end();
+    } else {
+        res.statusCode = 400; // 400 = Bad request
+        res.write(JSON.stringify({'msg': 'error'}));
+        res.end();
+    }
 });
 
-// server is listening to incoming requests on port 8080 on localhost
 server.listen(8080, function () {
-    console.log("Listening on port http://localhost:8080");
+    console.log('Listening on port http://localhost:8080');
 });
--- webapp/demo/src/canvas/canvas.tsx
+++ webapp/demo/src/canvas/canvas.tsx
@@ -1,55 +1,9 @@
 import '../App.css';
 
 function Canvas() {
-
-
-
-    // I think I could decode/inflate/etc a png file in a similar way to getting an image from below
-    /*{
-        fetch( 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png' )
-            .then( (r: Response) => r.arrayBuffer() )
-            .then( buffer => { // note this is already an ArrayBuffer
-                               // there is no buffer.data here
-                    const blob = new Blob( [ buffer ] );
-                    const url = URL.createObjectURL( blob );
-                    const img = document.getElementById( 'img' );
-                    console.log('buffer is:');
-                    console.log(buffer);
-                    console.log('url is:');
-                    console.log(url);
-                    console.log('img is:');
-                    console.log(img);
-                    // img.src = url; I commented this out
-                    // So the Blob can be Garbage Collected
-                    // img.onload = e => URL.revokeObjectURL( url );
-                    // ... do something else with 'buffer'
-                    } );
-    }*/
-
-    //
-    //
-    // why even bother with pngs at this point,
-    // just request to join a picture
-    // then, server gives image at a point in time
-    // and starts sending updates for anything after that
-    //
-    // it (drawing with canvas' draw functionality as events occur) (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/ellipse)
-    // kind of leaves the question of how to shove something in that
-    // was stuck in transit and is late. Ie how to draw something when
-    // a later event has already been drawn?
-    // Maybe just redraw from that event on? pretty mellow
-    //
-    // so I guess I'm drawing rectangles since that is easy
-    //
-    // I guess I can start with a server that can:
-    // 1. receive updates and apply them to a master thing
-    // 2. propagate updates
-    // 3. register and unregister clients from photos
-    // 4. send png of picture upon registration
-
   return (
     <div className="Canvas">
-
+        canvas
     </div>
   );
 }
