455a00fa2c021d31df74cf0766e8562e759b6cd1
TJ <tjbindseil@gmail.com>
2022-09-27 22:43:23 -0600

    write up post picture basics, need to get it to work

--- webapp/demo/src/components/new_picture_screen.tsx
+++ webapp/demo/src/components/new_picture_screen.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 
@@ -7,10 +8,65 @@ export function NewPictureScreen() {
         navigate(url);
     };
 
+    const [createdBy, setCreatedBy] = useState('');
+    const [pictureName, setPictureName] = useState('');
+
+    const handleCreatedByChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target;
+        const value = target.value;
+
+        setCreatedBy(value);
+    };
+
+    const handlePictureNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target;
+        const value = target.value;
+
+        setPictureName(value);
+    };
+
+    const createPicture = () => {
+            fetch('http://localhost:8080/picture', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({createdBy: createdBy, name: pictureName})
+            })
+                .then(result => result.json())
+                .then(
+                    result => console.log(`result is: ${result}`),
+                    error => console.log(`first catch and error is: ${error}`)
+                )
+                .catch(error => console.log(`last catch and error is: ${error}`));
+    };
+
     return (
-        <div className="NewPictureScreen">
+        <div>
+            <label>
+                {'Created By:'}
+            </label>
+            <input
+                type={"text"}
+                name={'createdBy'}
+                value={createdBy}
+                onChange={handleCreatedByChange}/>
+            <label>
+                {'Picture Name:'}
+            </label>
+            <input
+                type={"text"}
+                name={'pictureName'}
+                value={pictureName}
+                onChange={handlePictureNameChange}/>
+            <button
+                onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { void event; createPicture(); }}>
+                Create Picture
+            </button>
+
             <p><button onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { void event; go("/pictures");}}>Pictures</button></p>
             <p><button onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { void event; go("/new-picture");}}>New Picture</button></p>
         </div>
     );
 }
+                // placeHolder={'Created By:'}/>
+                // for={'createdBy'}>
