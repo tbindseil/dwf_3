7b4fd791a0b67731589e67c441460ffa9d0eeebf
TJ <tjbindseil@gmail.com>
2023-04-19 18:57:32 -0600

    save latest db schema

--- backend/database.ddl.sql
+++ backend/database.ddl.sql
@@ -7,5 +7,8 @@
 CREATE TABLE public.picture (
 	id serial4 NOT NULL,
 	"name" varchar NULL,
+	createdby varchar NULL,
+	filename varchar NULL,
+	filesystem varchar NULL,
 	CONSTRAINT picture_pk PRIMARY KEY (id)
 );
