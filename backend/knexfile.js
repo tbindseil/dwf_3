51a9f8c3e196d313129a407cbbc4ba22909a31c1
TJ <tjbindseil@gmail.com>
2023-05-04 20:40:21 -0600

    knex wip

---
+++ backend/knexfile.js
@@ -0,0 +1,10 @@
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+});
--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,5 +1,23 @@
 import { Pool } from 'pg';
 
+const pg = require('knex')({
+    client: 'pg',
+    // connection: process.env.PG_CONNECTION_STRING, not useful
+    searchPath: ['knex', 'public'],
+    connection: async () => {
+        // TODO determine dev or not
+        return {
+            debug: true,
+            asyncStackTraces: true
+        };
+    },
+
+    // set min to 0 so all idle connections can be terminated
+    pool: {
+        min: 0, max: 10
+    }
+});
+
 export default interface IDB {
   query: (text: string, params: string[]) => Promise<any>
 }
