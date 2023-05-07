551bc886590f71dd498fb1b0267039f162d7ff87
TJ <tjbindseil@gmail.com>
2023-05-07 16:47:24 -0600

    lint configuration, lots of issues to deal with

--- backend/test/integ/get_pictures.integ.test.ts
+++ backend/test/integ/get_pictures.integ.test.ts
@@ -8,6 +8,7 @@ import {GetPicturesOutput} from 'dwf-3-models-tjb';
 
 describe('get_pictures', () => {
     // TODO deal rename createdby to created_by and introduce a camel to snake decoder
+    // if this is a big deal, i gotta tack on a linter
     const expectedPictures = [
         { id: 1, name: "name1", createdBy: "createdb1", filename: "filenam1", filesystem: "filesyste1" },
         { id: 2, name: "name2", createdBy: "createdb2", filename: "filenam2", filesystem: "filesyste2" },
@@ -33,11 +34,8 @@ describe('get_pictures', () => {
 
     describe('GET /pictures', () => {
         it('should return pictures', async () => {
-            // used to be request(app), maybe need to export app even if it is server that is started? so expoert server, app, and io
             const { body: pictures } = await request(server).get('/pictures').expect(200)
             const getPicturesResponse = pictures as GetPicturesOutput;
-
-            // TODO more importantly, and deal with createdby vs createdBy ....
 
             expect(getPicturesResponse.pictures.length).toBe(expectedPictures.length);
             expect(pictures.pictures).toEqual(expectedPictures);
