104c4cb175bfd7ea321f4e941dfb2e3f39c27019
TJ <tjbindseil@gmail.com>
2023-06-02 20:40:44 -0600

    quick test that passes

--- backend/src/handlers/get_picture_objection.ts
+++ backend/src/handlers/get_picture_objection.ts
@@ -1,7 +1,6 @@
 import { GetPictureInput, GetPictureOutput, _schema } from 'dwf-3-models-tjb';
 import API from './api';
 import APIError from './api_error';
-import IDB from '../db';
 import PictureAccessor from '../picture_accessor/picture_accessor';
 import { NextFunction } from 'express';
 import { ValidateFunction } from 'ajv';
@@ -13,8 +12,8 @@ export class GetPictureObjection extends API<
 > {
     private readonly pictureAccessor: PictureAccessor;
 
-    constructor(db: IDB, pictureAccessor: PictureAccessor) {
-        super(db, 'GET', 'picture');
+    constructor(pictureAccessor: PictureAccessor) {
+        super();
 
         this.pictureAccessor = pictureAccessor;
     }
@@ -24,11 +23,9 @@ export class GetPictureObjection extends API<
     }
 
     public async process(
-        db: IDB,
         input: GetPictureInput,
         next: NextFunction
     ): Promise<GetPictureOutput> {
-        db;
         next;
 
         const query = PictureObjectionModel.query().findById(input.id);
@@ -41,24 +38,8 @@ export class GetPictureObjection extends API<
             );
         }
 
-        console.log(`@@@@ TJTAG @@@@ filename is ${filename}`);
-
         // TODO I think all api process methods throw 500 in their bodies, coudl be 500 by default
         return await this.pictureAccessor.getPicture(filename);
-
-        //         const query = 'select filename from picture where id = ?;';
-        //         const params = [input.id.toString()];
-        //
-        //         try {
-        //             const result = await db.query(query, params);
-        //             const filename = result.rows[0].filename;
-        //             return await this.pictureAccessor.getPicture(filename);
-        //         } catch (error) {
-        //             return this.handleError(
-        //                 new APIError(500, 'database issue, picture not fetched'),
-        //                 next
-        //             );
-        //         }
     }
 
     public getContentType(): string {
@@ -69,16 +50,3 @@ export class GetPictureObjection extends API<
         return output;
     }
 }
-//
-// const query = Person.relatedQuery('pets').for(ctx.params.id);
-//
-// if (ctx.query.name) {
-//     query.where('name', 'like', ctx.query.name);
-// }
-//
-// if (ctx.query.species) {
-//     query.where('species', ctx.query.species);
-// }
-//
-// const pets = await query;
-// ctx.body = pets;
