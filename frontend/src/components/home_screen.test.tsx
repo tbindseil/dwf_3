8f3def48db22ccfcafbf8cea4b8119eae65f115c
TJ <tjbindseil@gmail.com>
2023-07-04 13:34:04 -0600

    didn't work... may need to mock therouter like described in here: https://stackoverflow.com/questions/70313688/how-i-could-test-location-with-memoryrouter-on-react-router-dom-v6

--- frontend/src/components/home_screen.test.tsx
+++ frontend/src/components/home_screen.test.tsx
@@ -1,13 +1,19 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { HomeScreen } from './home_screen';
+
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory({ window });
 
 describe('HomeScreen tests', () => {
   beforeEach(() => {
     render(
-      <BrowserRouter>
+      // what would https://kentcdodds.com/blog/common-mistakes-with-react-testing-library say?
+      <HistoryRouter history={history}>
         <HomeScreen />
-      </BrowserRouter>,
+      </HistoryRouter>,
     );
   });
 
@@ -20,13 +26,17 @@ describe('HomeScreen tests', () => {
     // ...
   });
 
-  it('renders NewPictureScreen button', () => {
+  it('renders NewPictureScreen button', async () => {
     // const history = createMemoryHistory();
 
     const newPictureButton = screen.getByText('New Picture');
     expect(newPictureButton).toBeInTheDocument();
 
     fireEvent.click(newPictureButton);
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/new-picture');
+    });
 
     // expect(history.location.pathname).toBe('/location1');
 
