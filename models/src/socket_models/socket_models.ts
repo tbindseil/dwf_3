ab3545546cd8f4f50bb8a6e7d17d07784f3089f6
TJ <tjbindseil@gmail.com>
2023-07-28 16:50:18 -0600

    clean up comments from an old problem in socket_models

--- models/src/socket_models/socket_models.ts
+++ models/src/socket_models/socket_models.ts
@@ -1,7 +1,4 @@
-import { Namespace, Server, ServerOptions, Socket } from 'socket.io';
 import { PixelUpdate } from '../updates/pixel_update';
-import { Client } from 'socket.io/dist/client';
-import http from 'http';
 
 export interface ClientToServerEvents {
     picture_request: (pictureRequest: PictureRequest) => void;
@@ -23,77 +20,6 @@ export interface SocketData {
     name: string;
     age: number;
 }
-
-// the below seems pretty excessive, but I wasn't able to do type alias because of an issue instantiating
-//export class DWFServer extends Server<
-//    ClientToServerEvents,
-//    ServerToClientEvents,
-//    InterServerEvents,
-//    SocketData
-//> {
-//    constructor(srv: http.Server, opts?: Partial<ServerOptions> | undefined) {
-//        console.log('@@ TJTAG @@');
-//        super(srv, opts);
-//    }
-//}
-//
-//export class DWFSocket extends Socket<
-//    ClientToServerEvents,
-//    ServerToClientEvents,
-//    InterServerEvents,
-//    SocketData
-//> {
-//    constructor(
-//        namespace: Namespace<
-//            ClientToServerEvents,
-//            ServerToClientEvents,
-//            InterServerEvents,
-//            any
-//        >,
-//        client: Client<
-//            ClientToServerEvents,
-//            ServerToClientEvents,
-//            InterServerEvents,
-//            any
-//        >,
-//        auth: object
-//    ) {
-//        super(namespace, client, auth);
-//    }
-//}
-
-//export interface DWFServer
-//    extends Server<
-//        ClientToServerEvents,
-//        ServerToClientEvents,
-//        InterServerEvents,
-//        SocketData
-//    > {}
-//
-//export interface DWFSocket
-//    extends Socket<
-//        ServerToClientEvents,
-//        ClientToServerEvents,
-//        InterServerEvents,
-//        SocketData
-//    > {}
-
-//export type DWFServer = Server<
-//    ClientToServerEvents,
-//    ServerToClientEvents,
-//    InterServerEvents,
-//    SocketData
-//>;
-//
-//export type DWFSocket = Socket<
-//    ServerToClientEvents,
-//    ClientToServerEvents,
-//    InterServerEvents,
-//    SocketData
-//>;
-
-// see docs, but can utilize namespace specific event maps
-// export type DWFSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
 
 export interface PictureRequest {
     filename: string;
