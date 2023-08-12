0b9e1f8278eacb86fc878d42bd7f90b8072f4cb0
TJ <tjbindseil@gmail.com>
2023-08-11 21:50:48 -0600

    write tests

--- backend/test/integ/broadcast.test.ts
+++ backend/test/integ/broadcast.test.ts
@@ -1,106 +1,143 @@
-import { PixelUpdate } from 'dwf-3-models-tjb';
+import request from 'supertest';
+import * as fs from 'fs';
+
+import { io as io_package } from 'socket.io-client';
+
+import { PixelUpdate, PostPictureInput } from 'dwf-3-models-tjb';
+import { io, server } from '../../src/app';
+
+const ENDPOINT = 'http://127.0.0.1:6543/';
 
 const PICTURE_WIDTH = 800;
 const PICTURE_HEIGHT = 1000;
-
-describe('broadcast test', () => {
-
-  beforeEach(() => {
-
-  });
-
-  it('runs the test', () => {
-    const numClients = 3;
-    const numUpdates = [ 4, 1, 6];
-    testsFromRandom(numClients, numUpdates);
-  });
-});
-
-export const testsFromFile = (previousUpdatesFilename?: string) => {
-  let updates: Update[][] = recoverUpdates(previousUpdatesFilename);
-  // do it in here
-  tests(updates)
-}
-
-export const testsFromRandom = (numClients: number, numUpdates: number[]) => {
-  let updates: Update[][] = [];
-  for (let i = 0; i < numClients; ++i) {
-    for (let j = 0; j < numUpdates.get(i); ++j) {
-      updates.push(makeRandomUpdate());
-    }
-  }
-  tests(updates);
-}
-
-const tests = (updates: Update[][]) => {
-  // write first incase we crash
-  writeToFile(JSON.stringify(updates), 'savedUpdates.json' + timestamp);
-
-  const clients = Promise<void>[];
-  updates.forEach(updates => {
-    clients.push(spawnClient(updates));
-  }
-
-  Promise.awaitAll(clients);
-
-  // TODO await...
-
-
-  // do we want to kick them all off?
-  // naw, let that be part of the randomness for now
-  // TODO instead, more complicated, repeat them
-}
-
-import { io } from 'socket.io-client';
-const ENDPOINT = 'http://127.0.0.1:6543/';
-
-connectAsClient() {
-  return io(ENDPOINT);
-}
-
-async spawnClient(updates: Update[]): Promise<void> {
-  const socket = connectAsClient()
-
-  console.log(`spawning client with socketId: ${socket.id}`);
-
-  // need to forloop to serialize these
-  for (let i = 0; i < updates.lenth; ++i) {
-    let u = updates[i];
-    socket.emit('client_to_server_udpate', u.pixelUpdate);
-    await delay(u.waitTimeMS);
-  }
-
-  // TODO is this the right call?
-  socket.close();
-}
 
 interface Update {
   waitTimeMS: number;
   pixelUpdate: PixelUpdate;
 }
 
-const makeRandomUpdate = (clientNum: number): Update => {
-  const waitTimeMS = randomNumberBetweenZeroAnd(100);
-  const pixelUpdate = {
-    x: randomNumberBetweenZeroAnd(PICTURE_WIDTH),
-    y: randomNumberBetweenZeroAnd(PICTURE_HEIGHT),
-    red: randomNumberBetweenZeroAnd(255),
-    green: randomNumberBetweenZeroAnd(255),
-    blue: randomNumberBetweenZeroAnd(255),
-    filename: filename, 
-    createdBy: `client_${clientNum}`
+io.listen(6543);
+const port = process.env.PORT || 8080;
+// maybe i want to run this in a separate process since node is single threaded
+server.listen(port, () => {
+    // TODO wait until server is running
+    console.log(`Listening on port ${port}`);
+});
+
+describe('broadcast test', () => {
+  let testFilename: string;
+  const testPicture = {
+    name: 'name',
+    createdBy: 'createdBy',
+    width: PICTURE_WIDTH,
+    height: PICTURE_HEIGHT
   }
 
-  return {
-    waitTimeMS,
-    pixelUpdate
-  };
-}
+  beforeEach(async () => {
+    // create a picture and make sure its there
+    const payload: PostPictureInput = {
+      name: testPicture.name,
+      createdBy: testPicture.createdBy,
+      width: testPicture.width,
+      height: testPicture.height
+    };
 
-const randomNumberBetweenZeroAnd = (high: number): number => {
-  return Math.floor(high * Math.random());
-}
+    await request(server)
+    .post('/picture')
+    .send(payload)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .expect(200);
 
-const delay = (ms: number) => {
+
+    // look at all posted pictures
+    const { body: pictures } = await request(server)
+    .get('/pictures')
+    .expect(200);
+    expect(pictures.pictures.length).toEqual(1);
+
+    testFilename = pictures.pictures[0].filename;
+  });
+
+  it('runs the test', async () => {
+    const numClients = 3;
+    const numUpdates = [ 4, 1, 6];
+    await testsFromRandom(numClients, numUpdates);
+  });
+
+  const testsFromFile = (previousUpdatesFilename?: string) => {
+    let updates: Update[][] = [] // recoverUpdates(previousUpdatesFilename);
+    // do it in here
+    tests(updates)
+  }
+
+  const testsFromRandom = async (numClients: number, numUpdates: number[]) => {
+    let updates: Update[][] = [];
+    for (let i = 0; i < numClients; ++i) {
+      updates.push([]);
+      for (let j = 0; j < numUpdates[i]; ++j) {
+        updates[i].push(makeRandomUpdate(i));
+      }
+    }
+    await tests(updates);
+  }
+
+  const tests = async (updates: Update[][]) => {
+    // write first incase we crash
+    const createdAt = new Date().toString().replaceAll(' ', '__');
+    await fs.promises.writeFile(`savedTestUpdates_${createdAt}`, JSON.stringify(updates));
+
+    const clients: Promise<void>[] = [];
+    updates.forEach(updates => {
+      clients.push(spawnClient(updates));
+    })
+
+    await Promise.all(clients);
+
+
+    // do we want to kick them all off?
+    // naw, let that be part of the randomness for now
+    // TODO instead, more complicated, repeat them
+  }
+
+  const spawnClient = async (updates: Update[]): Promise<void> => {
+    const socket = io_package(ENDPOINT);
+
+    console.log(`spawning client with socketId: ${socket.id}`);
+
+    // need to forloop to serialize these
+    for (let i = 0; i < updates.length; ++i) {
+      let u = updates[i];
+      socket.emit('client_to_server_udpate', u.pixelUpdate);
+      await delay(u.waitTimeMS);
+    }
+
+    socket.close();
+  }
+
+  const makeRandomUpdate = (clientNum: number): Update => {
+    const waitTimeMS = randomNumberBetweenZeroAnd(100);
+    const pixelUpdate = {
+      x: randomNumberBetweenZeroAnd(PICTURE_WIDTH),
+      y: randomNumberBetweenZeroAnd(PICTURE_HEIGHT),
+      red: randomNumberBetweenZeroAnd(255),
+      green: randomNumberBetweenZeroAnd(255),
+      blue: randomNumberBetweenZeroAnd(255),
+      filename: testFilename,
+      createdBy: `client_${clientNum}`
+    }
+
+    return {
+      waitTimeMS,
+      pixelUpdate
+    };
+  }
+
+  const randomNumberBetweenZeroAnd = (high: number): number => {
+    return Math.floor(high * Math.random());
+  }
+
+  const delay = async (ms: number) => {
     await new Promise((r) => setTimeout(r, ms));
-}
+  }
+});
