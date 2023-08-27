209a6a3dfc7f267141e68f7e0f8520fb506f760f
TJ <tjbindseil@gmail.com>
2023-08-27 10:26:05 -0600

    start turning updates into classes, may need to rearrange some dependencies

--- models/src/updates/pixel_update.ts
+++ models/src/updates/pixel_update.ts
@@ -1,9 +1,43 @@
 import { Update } from './update';
 
-export interface PixelUpdate extends Update {
-    x: number,
-    y: number,
-    red: number,
-    green: number,
-    blue: number
+export class PixelUpdate extends Update {
+    public readonly x: number;
+    public readonly y: number;
+    public readonly red: number;
+    public readonly green: number;
+    public readonly blue: number;
+
+    public constructor(filename: string, createdBy: string, x: number, y: number, red: number, green: number, blue: number) {
+        super(filename, createdBy);
+        this.x = x;
+        this.y = y;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public updateRaster(raster: Raster) {
+        const imageDataOffset = 4 * (this.y * raster.width + this.x);
+        const red = this.clamp(this.red);
+        const green = this.clamp(this.green);
+        const blue = this.clamp(this.blue);
+
+        raster.getBuffer()[imageDataOffset] = red;
+        raster.getBuffer()[imageDataOffset + 1] = green;
+        raster.getBuffer()[imageDataOffset + 2] = blue;
+
+        console.log(`TJTAG updated at ${performance.now()}`);
+
+        // what if we did this as several blocks of arrays? for parallization?
+    }
+
+    private clamp(val: number, min = 0, max = 255): number {
+        if (val < min) {
+            return min;
+        } else if (val > max) {
+            return max;
+        } else {
+            return val;
+        }
+    }
 }
--- models/src/updates/update.ts
+++ models/src/updates/update.ts
@@ -1,5 +1,14 @@
-export interface Update {
-    guid: string,
-    filename: string,
-    createdBy: string,
+import {v4 as uuidv4} from 'uuid';
+
+export abstract class Update {
+    public readonly guid = uuidv4();
+    private readonly filename: string;
+    private readonly createdBy: string;
+
+    constructor(filename: string, createdBy: string) {
+        this.filename = filename;
+        this.createdBy = createdBy;
+    }
+
+    public abstract updateRaster(raster: Raster): void;
 }
