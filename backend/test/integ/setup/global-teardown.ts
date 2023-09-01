ed36c178728e3fca9d58db92990f076e8256d039
TJ <tjbindseil@gmail.com>
2023-09-01 11:01:03 -0500

    hmmm now it hangs also

--- backend/test/integ/setup/global-teardown.ts
+++ backend/test/integ/setup/global-teardown.ts
@@ -2,19 +2,29 @@ import {
     makeKnexWithoutDatabase,
     testDatabase,
 } from '../../../src/db/knex_file';
+import {server} from '../../../src/app';
 import { removeAllPng } from './utils';
 
 module.exports = async () => {
-    await removeAllPng();
+    const serverClosed = new Promise<void>(async (resolve) => {
+        await removeAllPng();
 
-    try {
-        const knexWithoutDatabase = makeKnexWithoutDatabase();
-        await knexWithoutDatabase.raw(
-            `DROP DATABASE IF EXISTS ${testDatabase}`
-        );
-        knexWithoutDatabase.destroy();
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+        try {
+            const knexWithoutDatabase = makeKnexWithoutDatabase();
+            await knexWithoutDatabase.raw(
+                `DROP DATABASE IF EXISTS ${testDatabase}`
+            );
+            knexWithoutDatabase.destroy();
+        } catch (error) {
+            console.log(error);
+            process.exit(1);
+        }
+
+        server.close(() => {
+            console.log('server closing');
+            resolve();
+        });
+    });
+
+    await serverClosed;
 };
