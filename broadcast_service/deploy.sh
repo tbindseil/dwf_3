54781db10d240afa3ee720c58937ea434c39691d
TJ <tjbindseil@gmail.com>
2023-10-14 12:31:53 -0600

    more stuff, I manually installed node on the ec2 pretty easily

--- broadcast_service/deploy.sh
+++ broadcast_service/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP_ADDRESS="44.215.67.60"
+IP_ADDRESS="3.235.90.59"
 
 set -e
 
@@ -34,6 +34,11 @@ scp -i "ec2-key-pair.pem" ./dwf_backend-1.0.0.tgz ec2-user@$IP_ADDRESS:/home/ec2
 #
 # I think I am suspecting that the scripts are only run when the resource doesn't already exist
 # but, I could've verified this by examinging the contents of the file where "it works" is echoed to
+
+# so, the failure script doesn't cause failure, and rerunning deployment does not lead to the "it works" text being modified
+
+# so, I'm just gonna install node once manually, then we will need to figure that out when containerizing this
+# followed https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html to do so
 
 # echo "checking node:"
 # let node_version=$(node -v)
--- cdk/lib/user-data.sh
+++ cdk/lib/user-data.sh
@@ -10,4 +10,4 @@ chmod 2775 /usr/share/nginx/html
 find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
 find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
 
-echo "<h1>It worked</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>It worked, again</h1>" > /usr/share/nginx/html/index.html
