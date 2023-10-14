c2e31437219547163826eba65bd53670f7b67c66
TJ <tjbindseil@gmail.com>
2023-10-14 12:49:56 -0600

    there's a lot going on with deploying the backend server, and i need to do the db first anyway

--- broadcast_service/deploy.sh
+++ broadcast_service/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP_ADDRESS="3.235.90.59"
+IP_ADDRESS="44.201.62.183"
 
 set -e
 
@@ -16,6 +16,7 @@ echo "linting:"
 # npm run lint
 
 echo "packaging:"
+# TODO more thorough instructions here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/deployment
 # npm pack
 
 echo "beaming artifacts to the cloud:"
@@ -39,6 +40,14 @@ scp -i "ec2-key-pair.pem" ./dwf_backend-1.0.0.tgz ec2-user@$IP_ADDRESS:/home/ec2
 
 # so, I'm just gonna install node once manually, then we will need to figure that out when containerizing this
 # followed https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html to do so
+
+# so i can install the .tgz file with this: https://stackoverflow.com/questions/51261784/npm-install-from-tgz-created-with-npm-pack
+# and I can run scripts with forever: https://www.npmjs.com/package/forever
+
+# what is the script I would invoke?
+
+# ok its kinda hard...
+# lets try to put the DB up there
 
 # echo "checking node:"
 # let node_version=$(node -v)
